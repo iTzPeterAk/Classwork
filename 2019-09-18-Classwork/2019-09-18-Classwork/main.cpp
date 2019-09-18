8cbd08bd5df9a99dc7bbe908c8ac1c421d2b7fe3
iTzPeterAk <55443376+iTzPeterAk@users.noreply.github.com>
2019-09-18 08:24:11 -0400

    Add files via upload

---
+++ 2019-09-18-Classwork/2019-09-18-Classwork/main.cpp
@@ -0,0 +1,12 @@
+// Peter Akindayo
+// Intor to Computer Science
+// Period 1 
+// Mr. Williams 
+// Classwork 2019-09-18 
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "What's poppin? :) XD" << std::endl;
+}
