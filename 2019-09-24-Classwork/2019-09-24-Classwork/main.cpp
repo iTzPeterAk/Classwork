4fe24c18ce6b33a0acaba97e1d5fc25eb7af51b7
iTzPeterAk <55443376+iTzPeterAk@users.noreply.github.com>
2019-09-24 08:45:42 -0400

    Add files via upload

---
+++ 2019-09-24-Classwork/2019-09-24-Classwork/main.cpp
@@ -0,0 +1,23 @@
+// Peter Akindayo
+// Intro to CS
+// Mr. Williams
+// Period 1.
+// 2019-09-24-Classwork
+
+#include <iostream>
+#include <array>
+
+int main()
+{
+	int why = 10; 
+    char dude = 7;
+	float yo = 6;
+	bool hold = 9;
+	double b = 5;
+	int x = 4;
+	int p = 3;
+	double e = 17; 
+	int t = 12;
+	int mk = 3;
+	std::cout << sizeof(why) << std::endl;
+}
