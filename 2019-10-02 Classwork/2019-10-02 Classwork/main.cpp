71b3a53242c2029771e16a1b0dc602d393634b6d
iTzPeterAk <55443376+iTzPeterAk@users.noreply.github.com>
2019-10-02 08:41:20 -0400

    Add files via upload

---
+++ 2019-10-02 Classwork/2019-10-02 Classwork/main.cpp
@@ -0,0 +1,20 @@
+// Peter Akindayo
+// Intro Computerto CS
+// Mr. Williams
+// Period 2
+// 2019-10-02 Classwork
+
+#include <iostream>
+int main()
+{
+	char letterA = 'A';
+	char b = 'h';
+	char why = '45';
+	char yo = '21';
+	// These are integers
+	short jeezy = '10';
+	int dude = '7';
+	unsigned long health = '23';
+
+
+}
