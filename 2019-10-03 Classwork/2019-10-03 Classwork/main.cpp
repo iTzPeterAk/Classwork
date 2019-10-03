b971de4ed357844153c63b3745c1cc1f876a5069
iTzPeterAk <55443376+iTzPeterAk@users.noreply.github.com>
2019-10-03 08:38:41 -0400

    Add files via upload

---
+++ 2019-10-03 Classwork/2019-10-03 Classwork/main.cpp
@@ -0,0 +1,16 @@
+// Peter Akindayo
+// Intro to CS
+// Period 1
+// Mr. Williams
+// 2019-10-03 Classwork
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << " Enter your favorite: ";
+	int favoriteNumber;
+	cin >> favoriteNumber;
+	cout << "Cool" << favoriteNumber << " is my favorite number too!" << endl;
+}
